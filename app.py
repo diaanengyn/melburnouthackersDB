fc485b8d2b66f3a1daa3dc8218df08ddcb001af5
dakshagrawal <dakshagrawalx@gmail.com>
2023-05-07 10:18:36 +1000

    Company

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
 from flask import Flask
 from inventory import inventory
 from shipment import shipment
+from step import step
+from company import company
 
 app = Flask(__name__)
 app.app_context().push()
 app.register_blueprint(inventory)
 app.register_blueprint(shipment)
+app.register_blueprint(step)
+app.register_blueprint(company)
 
 
 # Members API Route
